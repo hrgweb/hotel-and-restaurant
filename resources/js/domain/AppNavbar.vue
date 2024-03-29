6d050b619d938a4dd68fb74dae5f6cc058183dd1
Hergen Roque Gamayon <gamayonhergenroque.web@gmail.com>
2024-03-29 21:05:34 +0800

    sidebar icons

--- resources/js/domain/AppNavbar.vue
+++ resources/js/domain/AppNavbar.vue
@@ -2,27 +2,31 @@
   <div class="navbar">
     <a
       href="#"
-      class="text-gray-700 no-underline text-2xl font-bold text-center block py-3 bg-"
+      class="text-gray-700 no-underline text-2xl font-bold text-center block py-4 bg-"
       >HRG</a
     >
 
     <ul>
-      <li><a href="/dashboard">Dashboard</a></li>
-      <li><a href="/categories">Categories</a></li>
-      <li><a href="/products">Products</a></li>
-      <li><a href="/tables">Tables</a></li>
-      <li><a href="/user-roles">User Roles</a></li>
-      <li><a href="/staffs">Staffs</a></li>
-      <li><a href="/">Taxes</a></li>
-      <li><a href="/">Units</a></li>
-      <li><a href="/">Coupon</a></li>
-      <li><a href="/">Menu Items</a></li>
-      <li><a href="/">QR Code</a></li>
-      <li><a href="/">Order Management</a></li>
-      <li><a href="/">Stock Management</a></li>
-      <li><a href="/">Sales Report</a></li>
-      <li><a href="/">Drivers</a></li>
-      <li><a href="/">Feedback</a></li>
+      <li><i class="pi pi-home"></i> <a href="/dashboard">Dashboard</a></li>
+      <li>
+        <i class="pi pi-align-left"></i> <a href="/categories">Categories</a>
+      </li>
+      <li><i class="pi pi-palette"></i> <a href="/products">Products</a></li>
+      <li><i class="pi pi-table"></i> <a href="/tables">Tables</a></li>
+      <li>
+        <i class="pi pi-user-plus"></i> <a href="/user-roles">User Roles</a>
+      </li>
+      <li><i class="pi pi-users"></i> <a href="/staffs">Staffs</a></li>
+      <li><i class="pi pi-check"></i> <a href="/">Taxes</a></li>
+      <li><i class="pi pi-check"></i> <a href="/">Units</a></li>
+      <li><i class="pi pi-check"></i> <a href="/">Coupon</a></li>
+      <li><i class="pi pi-check"></i> <a href="/">Menu Items</a></li>
+      <li><i class="pi pi-check"></i> <a href="/">QR Code</a></li>
+      <li><i class="pi pi-check"></i> <a href="/">Order Management</a></li>
+      <li><i class="pi pi-check"></i> <a href="/">Stock Management</a></li>
+      <li><i class="pi pi-check"></i> <a href="/">Sales Report</a></li>
+      <li><i class="pi pi-check"></i> <a href="/">Drivers</a></li>
+      <li><i class="pi pi-check"></i> <a href="/">Feedback</a></li>
     </ul>
   </div>
 </template>
@@ -31,12 +35,19 @@
 
 <style scoped>
 .navbar {
+  a {
+    color: #4b5563;
+  }
+
   ul {
     padding: 0;
     margin: 0;
 
     & > li {
       list-style: none;
+      display: flex;
+      align-items: center;
+      padding-left: 1.5rem;
 
       & > a {
         padding: 0.8rem;
